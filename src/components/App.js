da435ef95f4297763d98cb822e10df62277b29c0
Christina Dob <cd@cd.local>
2019-03-22 16:27:59 -0700

    Week 3 Day 5 Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,71 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post';
 
 import './App.css';
-
+import { ToastContainer, toast } from 'react-toastify';
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios
+      .get(`https://practiceapi.devmountain.com/api/posts`)
+      .then(res => {
+        toast.success(`Success`);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(() => toast.error(`Error`));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(res => {
+        toast.success(`Success`);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(() => toast.error(`Error`));
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(res => {
+        toast.success(`Success`);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(() => toast.error(`Error`));
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post(`https://practiceapi.devmountain.com/api/posts`, { text })
+      .then(res => {
+        toast.success(`Success`);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(() => toast.error(`Error`));
   }
 
   render() {
@@ -42,9 +76,17 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              text={post.text}
+              date={post.date}
+              id={post.id}
+              updatePostFn={this.updatePost}
+              deletePostFn={this.deletePost}
+            />
+          ))}
         </section>
       </div>
     );
